4e7f74f13254c60e45c4d557a3aa419c673ce7ac
Justin Klüver <justin.k@justplayer.de>
2020-05-24 20:59:55 +0200

    Fallback "launcher"

---
+++ lua/autorun/libgmodstore_init.lua
@@ -0,0 +1,23 @@
+local DEBUGGING = false
+if (libgmodstore and not DEBUGGING) then return end -- We don't want to be running multiple times if we've already initialised
+libgmodstore = {}
+
+function libgmodstore:print(msg, type)
+    if (type == "error" or type == "bad") then
+        MsgC(Color(255, 0, 0), "[libgmodstore] ", Color(255, 255, 255), msg .. "\n")
+    elseif (type == "success" or type == "good") then
+        MsgC(Color(0, 255, 0), "[libgmodstore] ", Color(255, 255, 255), msg .. "\n")
+    else
+        MsgC(Color(0, 255, 255), "[libgmodstore] ", Color(255, 255, 255), msg .. "\n")
+    end
+end
+
+function libgmodstore:LoadBackup()
+    AddCSLuaFile("libgmodstore/libgmodstore.lua")
+    include("libgmodstore/libgmodstore.lua")
+end
+
+hook.Add("Think", "LibGmodstore Init", function()
+    libgmodstore:LoadBackup()
+    hook.Remove("Think", "LibGmodstore Init")
+end)

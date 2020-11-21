6dd8807176cb1a24dc8ba79b8edf0051a5935d27
Justin Klüver <justin.k@justplayer.de>
2020-11-21 03:55:34 +0100

    Better file names

--- lua/autorun/libgmodstore_autorun.lua
+++ lua/autorun/libgmodstore_autorun.lua
@@ -1,10 +1,10 @@
 hook.Add("Think", "LibGmodstore Init", function()
-    AddCSLuaFile("libgmodstore/init.lua")
-    AddCSLuaFile("libgmodstore/client.lua")
-    include("libgmodstore/init.lua")
+    AddCSLuaFile("libgmodstore/sh_init.lua")
+    AddCSLuaFile("libgmodstore/cl_ui.lua")
+    include("libgmodstore/sh_init.lua")
 
     if CLIENT then
-        include("libgmodstore/client.lua")
+        include("libgmodstore/cl_ui.lua")
     end
 
     hook.Remove("Think", "LibGmodstore Init")
